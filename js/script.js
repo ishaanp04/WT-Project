767cf54b42d0dcfae3cedc8af1e6ef4af51030b8
Ishaan Pandey <ishaanpandey04@gmail.com>
2023-11-01 10:49:10 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function home() {
+  document.getElementById("main-image").innerHTML =
+    '<img src="images/looking right.JPG" class="main-image" alt="" />';
+}
+
+function knowMe() {
+  document.getElementById("main-image").innerHTML =
+    '<img src="images/looking right.JPG" class="main-image" alt="" style=" position: relative; animation-name: move-up; animation-duration: 1.5s; animation-fill-mode: forwards;"/>';
+}
+
+function contact() {
+  document.getElementById("second-section").innerHTML =
+    '<img src="images/looking right.JPG" class="main-image" alt="" />';
+}
